e727ad5ad8f1d078001c8fd8ec39e9ed89c17349
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-02-16 20:36:25 +0000

    You must bring in the 'a' helpers if you want to use the a_ function
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheFeedbackPlugin/trunk@3370 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aFeedback/templates/_feedbackEmailText.php
+++ modules/aFeedback/templates/_feedbackEmailText.php
@@ -1,3 +1,4 @@
+<?php use_helper('a') ?>
 <?php echo a_('URL where the problem occured:') ?> http://<?php echo $_SERVER['HTTP_HOST'] ?><?php echo $feedback['section'] ?> 
 
 <?php echo $feedback['description'] ?>
